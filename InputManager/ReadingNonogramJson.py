b3aabca55346eedb551385b81bc1f02ed33a884a
Omri <benaomri@post.bgu.ac.il>
2023-01-14 18:00:30 +0200

    finished README.md

--- InputManager/ReadingNonogramJson.py
+++ InputManager/ReadingNonogramJson.py
@@ -1,8 +1,8 @@
 import json
-import main
 
 
 def process_json_from_file(filepath):
+    nonograms = []
     with open(filepath, "r") as file:
         json_string = file.read()
         try:
@@ -15,11 +15,7 @@ def process_json_from_file(filepath):
                     return
                 print("Row clues:", row_clues)
                 print("Column clues:", col_clues)
-                main.main(row_clues, col_clues)
+                nonograms.append([row_clues, col_clues])
         except json.decoder.JSONDecodeError as e:
             print(f'Error: {e}')
-
-
-if __name__ == '__main__':
-    filepath1 = "InputManager/nonogram_clues.json"
-    process_json_from_file(filepath1)
+    return nonograms
--- main.py
+++ main.py
@@ -8,16 +8,15 @@ from NonogramSolver.NonogramCrossover import NonogramCrossover
 from NonogramSolver.NonogramVectorCreator import NonogramVectorCreator
 from NonogramSolver.NonogramEvaluator import NonogramEvaluator
 import numpy as np
-
+from InputManager.ReadingNonogramJson import process_json_from_file
 from RunStatistics import RunStatistics
 
-POP_SIZE = 200
 MAX_GEN = 200
-NUM_EXPERIMENTS = 10
+NUM_EXPERIMENTS = 5
 HIGHER_IS_BETTER = True
 
 
-def main(row_clues, col_clues):
+def solve_nonogram(row_clues, col_clues):
     # Initialize the evolutionary algorithm
 
     # example:
@@ -31,6 +30,8 @@ def main(row_clues, col_clues):
     #  [ 1., -1.,  1.,  1., -1.],
     #  [-1., -1.,  1.,  1., -1.],
     #  [ 1., -1.,  1., -1.,  1.]])
+    N = len(col_clues)
+    POP_SIZE = N * 10
     clues = np.asarray([col_clues, row_clues], dtype=object)
     experiment_manager = ExperimentManager(total_num_experiments=NUM_EXPERIMENTS)
     for EXPERIMENT_COUNT in range(NUM_EXPERIMENTS):
@@ -61,7 +62,7 @@ def main(row_clues, col_clues):
                                      with_stat_prints=False,
                                      experiment_manager=experiment_manager),
             # We will change the seed to be a new seed in every experiment, since we don't want duplicate results
-            random_seed=EXPERIMENT_COUNT+1
+            random_seed=EXPERIMENT_COUNT + 1
         )
 
         algo.evolve()
@@ -74,4 +75,7 @@ def main(row_clues, col_clues):
 
 if __name__ == '__main__':
 
-    main(row_clues, col_clues)
+    nonograms_path = "InputManager/nonogram_clues.json"
+    nonograms = process_json_from_file(nonograms_path)
+    for nonogram in nonograms:
+        solve_nonogram(nonogram[0], nonogram[1])
